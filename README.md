4a5f6693a214f71209e9e97c16db7bd44cc5864a
Ben Falk <falk.ben@gmail.com>
2019-05-21 17:50:13 -0400

    init outline & lib

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # boss-export
+
 Export data from BOSS in bulk from w/in BOSS AWS environment
+
+A collection of scripts to be run from within the BOSS AWS environment to grab the cuboids from S3 and export them to an external bucket as neuroglancer precomputed format.
---
+++ boss_export/libs/mortonxyz.py
@@ -0,0 +1,16 @@
+"""
+methods to calculate morton index
+"""
+
+import morton  # https://pypi.python.org/pypi/morton-py/1.2
+
+def XYZMorton(x, y, z):
+    """ Get morton order from XYZ coordinates """
+    m = morton.Morton(dimensions=3, bits=64)
+    return m.pack(x, y, z)
+
+
+def MortonXYZ(zindex):
+    """ Get XYZ coordinates from morton order """
+    m = morton.Morton(dimensions=3, bits=64)
+    return m.unpack(zindex)
