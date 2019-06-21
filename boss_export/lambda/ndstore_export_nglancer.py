6104a99a378c3fe5396dda0d1643540fb1484a45
Ben Falk <falk.ben@gmail.com>
2019-06-21 16:44:47 -0400

    added functions to ndstore lib and tests
    lambda for ndstore2nglancer ready for testing

---
+++ boss_export/lambda/ndstore_export_nglancer.py
@@ -0,0 +1,85 @@
+"""Lambda to export data from ndstore to BOSS
+Entry point is an object inside ndstore
+Output is a neuroglancer gzip compressed object at the correct path and bucket
+
+# Needs the following access:
+
+s3 read access to ndstore bucket: neurodata-cuboid-store (s3neurodata)
+s3 write access to nd-precomputed-volumes (ICC)
+
+"""
+
+import boto3
+
+from boss_export.libs import ndstore, mortonxyz, ngprecomputed
+
+# TODO: dynamically calculate these
+DEST_BUCKET = "nd-precomputed-volumes"
+DEST_DATASET = "bock11_test"
+DEST_LAYER = "image_test"
+BASE_SCALE = 4, 4, 40
+CUBE_SIZE = 512, 512, 64
+dtype = "uint8"
+EXTENT = 135424, 119808, 4156  # x, y, z
+OFFSET = 0, 0, 2917
+
+# will get credentials from role it's running under
+S3_RESOURCE = boto3.resource("s3")
+
+
+def lambda_handler(event, context):
+    # Parse job parameters
+    invocationSchemaVersion = event["invocationSchemaVersion"]
+    invocationId = event["invocationId"]
+
+    # Process the task
+    task = event["tasks"][0]
+    taskId = task["taskId"]
+    s3Key = task["s3Key"]
+    s3BucketArn = task["s3BucketArn"]
+    s3Bucket = s3BucketArn.split(":")[-1]
+    print("BatchProcessObject(" + s3Bucket + "/" + s3Key + ")")
+
+    # object naming
+    # - decode the object name into its parts: morton ID, res, table keys
+    ndstorekey = ndstore.parts_from_ndstorekey(s3Key)
+
+    # get obj
+    # decompress the object from boss format to numpy
+    data_array = ndstore.get_block(s3Key, S3_RESOURCE, s3Bucket)
+
+    # get the coordinates of the cube
+    xyz_coords = mortonxyz.get_coords(ndstorekey.mortonid, CUBE_SIZE)
+
+    # need to reshape and reset size when at edges
+    data_array = ngprecomputed.crop_to_extent(data_array, xyz_coords, EXTENT)
+
+    # TODO: handle scale here for res > 0
+    # TODO: deal with morton offsets (boss has it, ngprecomputed does not)
+
+    # get the shape of the object
+    shape = data_array.shape
+
+    # compute neuroglancer key (w/ offset in name)
+    chunk_name = ngprecomputed.get_chunk_name(
+        ndstorekey.mortonid, BASE_SCALE, ndstorekey.res, shape, OFFSET
+    )
+    ngkey = ngprecomputed.get_ng_key(DEST_DATASET, DEST_LAYER, chunk_name)
+
+    # saving it out
+    # compress the object to neuroglancer format (gzip serialized numpy)
+    ngdata = ngprecomputed.numpy_chunk(data_array)
+
+    # save object to the target bucket and path
+    ngprecomputed.save_obj(S3_RESOURCE, DEST_BUCKET, ngkey, ngdata)
+
+    results = [
+        {"taskId": taskId, "resultCode": "Succeeded", "resultString": "Succeeded"}
+    ]
+
+    return {
+        "invocationSchemaVersion": invocationSchemaVersion,
+        "treatMissingKeysAs": "PermanentFailure",  # other options are "Successful", "TemporaryFailure"
+        "invocationId": invocationId,
+        "results": results,
+    }
--- boss_export/libs/ndstore.py
+++ boss_export/libs/ndstore.py
@@ -1,9 +1,16 @@
 import hashlib
+import time
+from dataclasses import dataclass
+
+import blosc
+from botocore.exceptions import ClientError
 
 from boss_export.libs import mortonxyz
 
 
 def calc_s3_key(project, channel, morton_index, res, time_index):
+    """Returns an s3 key from morton_index and project params
+    """
     hashm = hashlib.md5()
 
     end_str = "{}&{}&{}&{}&{}".format(project, channel, res, morton_index, time_index)
@@ -15,20 +22,57 @@ def calc_s3_key(project, channel, morton_index, res, time_index):
 
 
 def returns3key(project, channel, res, time_index, x, y, z):
+    """Returns an s3 key from xyz and project params
+    """
     m_idx = mortonxyz.XYZMorton(x, y, z)
     return calc_s3_key(project, channel, m_idx, res, time_index)
 
 
-def returnxyz(s3key):
-    # returns list x,y,z,res
+@dataclass
+class NdstoreKey:
+    s3key: str
+    digest: str
+    project: str
+    channel: str
+    res: int
+    mortonid: int
+    t: int
 
-    s3key_parts = s3key.split("&")
 
-    res = int(s3key_parts[3])
+def parts_from_ndstorekey(s3key):
+    """Returns: NdstoreKey
+    Note: parent_iso not returned if not in original s3key
+    REF:
+    s3key = "0000269c434123d23c0160b33025ba17&bock11&image&0&3090486&0"
+    HASH_PROJECT_CHANNEL_RES_MORTON_TIME
+    """
+    s3keyparts = s3key.split("&")
 
-    morton_idx = int(s3key_parts[4])
+    assert len(s3keyparts) == 6
 
-    x, y, z = mortonxyz.MortonXYZ(morton_idx)
+    # pop the strings
+    digest = s3keyparts.pop(0)
+    project = s3keyparts.pop(0)
+    channel = s3keyparts.pop(0)
 
-    return morton_idx, x, y, z, res
+    # rest of args are integers (res, mortonid, t)
+    s3keyparts = [int(p) for p in s3keyparts]
 
+    ndstorekey = NdstoreKey(s3key, digest, project, channel, *s3keyparts)
+    return ndstorekey
+
+
+def get_block(s3key, s3_resource, bucketname):
+    """Gets a cuboid from ndstore
+    s3_resource is a boto3 s3 resource
+    """
+    try:
+        s3_obj = s3_resource.Object(bucketname, s3key)
+        response = s3_obj.get()
+        blosc_data = response["Body"].read()
+
+        # unpack the blosc encoded data
+        return blosc.unpack_array(blosc_data, encoding="latin1")
+
+    except ClientError:
+        return None
