fdad1c938eda9b6ebb6aed24e4378b591390e709
Ben Falk <falk.ben@jhu.edu>
2019-11-22 12:23:01 -0500

    update process_deadletter logging

--- scripts/process_deadletter.py
+++ scripts/process_deadletter.py
@@ -1,14 +1,13 @@
 #%%
 import datetime
+import logging
 import time
-
+import os
 
 import boto3
 
-#%%
 SESSION = boto3.Session(profile_name="ben-boss-dev")
 SQS = SESSION.resource("sqs", region_name="us-east-1")
-
 SQS_NAME = "copy-boss-cuboids"
 queue = SQS.get_queue_by_name(QueueName=SQS_NAME)
 
@@ -18,38 +17,43 @@ deadletter_queue = SQS.get_queue_by_name(QueueName=DEADLETTER_SQS_NAME)
 
 #%%
 now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-with open(
-    f"/home/ben/repos/boss-export/scripts/logs/deadletter_{now}.log", "a"
-) as deadletterlogfile:
-    ntries = 0
-    while True:
-        msg_count = 0
-        entries, delete_entries = [], []
-        for message in deadletter_queue.receive_messages(MaxNumberOfMessages=10):
-            msg = message.body
-            deadletterlogfile.write(msg + "\n")
+logfname = f"logs/{now}.log"
+os.makedirs(os.path.dirname(logfname), exist_ok=True)
 
-            Id = message.message_id
-            entry = {"Id": Id, "MessageBody": msg}
-            entries.append(entry)
+logging.basicConfig(filename=logfname, filemode="w")
+logging.info(f"Log started. Time is {now}")
 
-            delete_entries.append(
-                {"Id": message.message_id, "ReceiptHandle": message.receipt_handle}
-            )
+#%%
+ntries = 0
+while True:
+    msg_count = 0
+    entries, delete_entries = [], []
+    for message in deadletter_queue.receive_messages(MaxNumberOfMessages=10):
+        msg = message.body
 
-            msg_count += 1
+        logging.info(msg)
 
-        if msg_count == 0:
-            if ntries > 2:
-                break
-            else:
-                print("got no messages, waiting 2 secs before trying again...")
-                time.sleep(2)
-                ntries += 1
+        Id = message.message_id
+        entry = {"Id": Id, "MessageBody": msg}
+        entries.append(entry)
+
+        delete_entries.append(
+            {"Id": message.message_id, "ReceiptHandle": message.receipt_handle}
+        )
+
+        msg_count += 1
+
+    if msg_count == 0:
+        if ntries > 2:
+            break
         else:
-            queue.send_messages(Entries=entries)
+            print("got no messages, waiting 2 secs before trying again...")
+            time.sleep(2)
+            ntries += 1
+    else:
+        queue.send_messages(Entries=entries)
 
-            deadletter_queue.delete_messages(Entries=delete_entries)
+        deadletter_queue.delete_messages(Entries=delete_entries)
 
 
 # %%
